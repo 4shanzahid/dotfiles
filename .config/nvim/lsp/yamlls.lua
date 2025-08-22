100a86e2fdf3ece4e28e23027876e5589285c98f
Ashan <ashanzahid@protonmail.com>
2025-08-22 16:54:54 +0500

    added new configs

--- .config/nvim/lsp/yamlls.lua
+++ .config/nvim/lsp/yamlls.lua
@@ -13,13 +13,49 @@ return {
 			completion = true,
 			validate = true,
 			schemaStore = {
-				enable = true,
-				url = "https://www.schemastore.org/json",
+				enable = false,
+				url = "",
 			},
+			-- Specific schemas for Docker and GitHub Actions
 			schemas = {
-				kubernetes = "*.yaml",
-				["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-				["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+				-- GitHub Actions Workflows
+				["https://json.schemastore.org/github-workflow.json"] = {
+					".github/workflows/*.yml",
+					".github/workflows/*.yaml",
+				},
+				-- GitHub Action metadata
+				["https://json.schemastore.org/github-action.json"] = {
+					".github/action.yml",
+					".github/action.yaml",
+					"action.yml",
+					"action.yaml",
+				},
+				-- Docker Compose - using the official Docker schema
+				["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = {
+					"docker-compose*.yml",
+					"docker-compose*.yaml",
+					"compose.yml",
+					"compose.yaml",
+				},
+				-- Alternative Docker Compose schema (backup)
+				["https://json.schemastore.org/docker-compose.json"] = {
+					"docker-compose.yml",
+					"docker-compose.yaml",
+				},
+			},
+			-- Custom tags support (useful for Docker Compose)
+			customTags = {
+				"!Ref",
+				"!Reference sequence",
+				"!reference",
+				"!include",
+				"!extends",
+			},
+		},
+		-- Disable RedHat telemetry
+		redhat = {
+			telemetry = {
+				enabled = false,
 			},
 		},
 	},
